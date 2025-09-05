9eb78fbbd9e54ead350a02f793c6b0483b557f9a
KuitoInoguchi <tstnamiou@gmail.com>
2025-09-05 22:47:12 +0800

    is prime finished

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,28 @@
-#include <printf.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+bool is_prime(const int n) {
+    if (n <= 1) {
+        return false;
+    }
+    const int bound = (int)sqrt(n);
+    for (int i = 2; i <= bound; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main() {
-    printf("hello world!\n");
+    int n = 0;
+    scanf("%d", &n);
+    if (is_prime(n)) {
+        printf("yes");
+    }
+    else {
+        printf("no");
+    }
     return 0;
 }
