0d087dee6657f36ca277d22f1b89df178bae0562
KuitoInoguchi <tstnamiou@gmail.com>
2025-09-05 15:22:09 +0800

    running letter finished

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,31 @@
-#include <printf.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void move_forwards(const int ScreenWidth) {
+    for (int i = 0; i < ScreenWidth; i++) {
+        system("cls"); // clear the screen
+        for (int j = 0; j < i; j++) {
+            printf(" ");
+        }
+        printf("a");
+    }
+}
+
+void move_backwards(const int ScreenWidth) {
+    for (int i = ScreenWidth; i > 0; i--) {
+        system("cls");
+        for (int j = 0; j < i; j++) {
+            printf(" ");
+        }
+        printf("a");
+    }
+}
 
 int main() {
-    printf("hello world!\n");
+    const int SCREEN_WIDTH = 50;
+    for (;;) {
+        move_forwards(SCREEN_WIDTH);
+        move_backwards(SCREEN_WIDTH);
+    }
     return 0;
 }
