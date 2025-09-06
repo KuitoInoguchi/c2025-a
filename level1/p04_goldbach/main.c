921e7ca0e857bb9edecdf1514e77dc855680bd2a
KuitoInoguchi <tstnamiou@gmail.com>
2025-09-06 23:10:16 +0800

    goldbach finished

--- level1/p04_goldbach/main.c
+++ level1/p04_goldbach/main.c
@@ -1,6 +1,40 @@
-#include <printf.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+    43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
+const size_t LEN = sizeof(primes) / sizeof(int);
+
+bool is_prime(const int n) {
+    for (int i = 0; i < LEN; i++) {
+        if (n == primes[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool prove_goldbach() {
+    for (int i = 4; i <= 100; i += 2) {
+        int index = 0;
+        for (int j = primes[index]; index < LEN; j = primes[++index]) {
+            if (is_prime(i - j)) {
+                break;
+            }
+            if (index == LEN - 1) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 int main() {
-    printf("hello world!\n");
+    if (prove_goldbach()) {
+        printf("yes");
+    }
+    else {
+        printf("no");
+    }
     return 0;
 }
